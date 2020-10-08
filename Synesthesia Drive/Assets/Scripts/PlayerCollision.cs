2ff6e4aca52abaae2e7ff34a9c435006d745bc79
Kirsten Magnus T. Untal <kirsten.untal@obf.ateneo.edu>
2020-10-09 02:32:52 +0800

    Added collision and obstacles

---
+++ Synesthesia Drive/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+	public CarMovement movement;
+
+    void OnCollisionEnter (Collision collisionInfo){
+
+    	if (collisionInfo.collider.tag =="Obstacle"){
+    		movement.enabled = false;
+    	}
+    }
+}

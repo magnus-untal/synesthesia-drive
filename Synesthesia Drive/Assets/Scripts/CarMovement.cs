8505ba3768c0cac2ccbc087ed873bad90e772155
Kirsten Magnus T. Untal <kirsten.untal@obf.ateneo.edu>
2020-10-08 23:15:41 +0800

    Initialized Base Tutorial Level
    Basic movement, camera, assets, and scripts added

---
+++ Synesthesia Drive/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+
+	public Rigidbody rb;
+    public float forwardForce = 1000f;
+    public Transform vehicle;
+    // FixedUpdate works better with Physics
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("a")){
+        	vehicle.position = new Vector3(-10, 0, vehicle.position.z);
+        }
+        if (Input.GetKey("s")){
+        	vehicle.position = new Vector3(0, 0, vehicle.position.z);
+        }
+        if (Input.GetKey("d")){
+        	vehicle.position = new Vector3(10, 0, vehicle.position.z);
+        }
+    }
+}

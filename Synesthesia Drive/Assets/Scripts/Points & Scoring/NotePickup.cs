b693740470c9661b068ecf36f6607f8809a7044d
GabeEscalaw <elise.escalaw@obf.ateneo.edu>
2020-10-16 22:02:06 +0800

    Updated Visuals
    > Implemented Universal Render Pipeline
    > Everything has actual applied lighting now
    > Added Post-Processing Effects
    > Added particle effects to notes when collecting
    > Changed the font and text locations
    > Baked the lighting (I think it might help with performance?)
    > Fixed the angle of the directional light so it matches the sunset in the horizon
    > Applied different textures to the obstacles because it couldn't adjust to the new pipeline
    > Applied a different skybox

--- Synesthesia Drive/Assets/Scripts/Points & Scoring/NotePickup.cs
+++ Synesthesia Drive/Assets/Scripts/Points & Scoring/NotePickup.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class NotePickup : MonoBehaviour
 {
+    public Transform noteEffect;
+
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other)
     {
         ScoreSystem.theStreak += 1;
         ScoreSystem.theScore += 100*ScoreSystem.multiplier;
+        Transform effect = (Transform)Instantiate(noteEffect, transform.position, transform.rotation);
+        Destroy(effect.gameObject, 3);
         Destroy(gameObject);
     }
 }

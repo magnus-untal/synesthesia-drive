7300695515103927f43e1a9f53b4a4f9ab7ef1f9
Kirsten Magnus T. Untal <kirsten.untal@obf.ateneo.edu>
2021-03-26 14:12:24 +0800

    Revert "Revert "Added First Song Map""
    This reverts commit 709edd40ccd223716fab8368b4c486df61731a05.

--- Synesthesia Drive/Assets/Scripts/CarMovement.cs
+++ Synesthesia Drive/Assets/Scripts/CarMovement.cs
@@ -9,16 +9,16 @@ public class CarMovement : MonoBehaviour
     // FixedUpdate works better with Physics
     void FixedUpdate()
     {
-        vehicle.Translate(Vector3.forward * 100 * Time.deltaTime);
+        vehicle.Translate(Vector3.forward * forwardForce * Time.deltaTime);
 
         if (Input.GetKey("a")){
-        	vehicle.position = new Vector3(-10, 0, vehicle.position.z);
+        	vehicle.position = new Vector3(480, 3.5f, vehicle.position.z);
         }
         if (Input.GetKey("s")){
-        	vehicle.position = new Vector3(0, 0, vehicle.position.z);
+        	vehicle.position = new Vector3(500, 3.5f, vehicle.position.z);
         }
         if (Input.GetKey("d")){
-        	vehicle.position = new Vector3(10, 0, vehicle.position.z);
+        	vehicle.position = new Vector3(520, 3.5f, vehicle.position.z);
         }
     }
 }
---
+++ Synesthesia Drive/Assets/Scripts/CarTag.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarTag : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision){
+        //Debug.Log("collided with " + collision.gameObject.name);
+    }
+}

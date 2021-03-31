f64fcdcb49f6077fda77e2b763e241a439521368
EugeneTiongco <38515176+EugeneTiongco@users.noreply.github.com>
2021-03-31 17:15:29 +0800

    Updated Wall Collision Mechanics
    -Moved wall hit detection to wall prefabs
    -added color change when hit

---
+++ Synesthesia Drive/Assets/Scripts/Points & Scoring/WallCollide.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCollide : MonoBehaviour
+{
+    Renderer rend;
+    Color hit = new Color(0, 0, 0, 0.5f);
+    Color start = new Color(1, 1, 1, 1);
+
+    
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.SetColor("_Color", start);
+    }
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        ScoreSystem.theStreak = 0;
+        ScoreSystem.multiplier = 1;
+      
+        rend.material.SetColor("_Color", hit);
+    }
+}
